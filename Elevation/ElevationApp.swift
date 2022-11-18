f81bb9ae421bcc29b4bc367b8947f7a17a7b182c
Jerome <jerome525@gmail.com>
2022-11-17 19:09:41 -0700

    Initial Commit

---
+++ Elevation/ElevationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevationApp.swift
+//  Elevation
+//
+//  Created by jerome on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ElevationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
